800df42e4731b3ca8be2d2b402b1cffee61b4199
Paul Le <nthpaul@proton.me>
2025-04-01 19:57:58 +1100

    day1: formatter and break before sentiment check

--- day-1/client.py
+++ day-1/client.py
@@ -1,20 +1,21 @@
 import socket
 
-def tcp_client(message, host='127.0.0.1', port=65432):
+
+def tcp_client(message, host="127.0.0.1", port=65432):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((host, port))
 
-    client.send(message.encode('utf-8'))
+    client.send(message.encode("utf-8"))
 
-    response = client.recv(1024).decode('utf-8')
+    response = client.recv(1024).decode("utf-8")
     print(f"Server response: {response}")
 
     client.close()
 
+
 if __name__ == "__main__":
     while True:
         message = input("Enter a message: ")
-        tcp_client(message)
-
         if message == "exit":
             break
+        tcp_client(message)
--- day-1/server.py
+++ day-1/server.py
@@ -3,7 +3,8 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 analyzer = SentimentIntensityAnalyzer()
 
-def start_tcp_server(host='127.0.0.1', port=65432):
+
+def start_tcp_server(host="127.0.0.1", port=65432):
     # AF_INET specifies IPv4 addresses
     # SOCK_STREAM specifies TCP, in-order, reliable, two-way connection-based byte streams
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,13 +12,13 @@ def start_tcp_server(host='127.0.0.1', port=65432):
     server.listen(5)
     print(f"Server listening on {host}:{port}")
 
-    while True: 
+    while True:
         client, addr = server.accept()
         print(f"Connected to {addr}")
 
         try:
-            message = client.recv(1024).decode('utf-8')
-            
+            message = client.recv(1024).decode("utf-8")
+
             if message:
                 print(f"Received message: {message}")
 
@@ -25,21 +26,22 @@ def start_tcp_server(host='127.0.0.1', port=65432):
                 valence = analyze_sentiment(message)
                 print(f"Sentiment: {valence}")
 
-                client.send(str(valence).encode('utf-8'))
+                client.send(str(valence).encode("utf-8"))
             else:
                 print("No message received")
-                client.sendall("No message received".encode('utf-8'))
+                client.sendall("No message received".encode("utf-8"))
 
         except Exception as e:
             print(f"Error: {e}")
-        
+
         finally:
             client.close()
+
 
 def analyze_sentiment(message):
     scores = analyzer.polarity_scores(message)
     print("scores", scores)
-    compound_score = scores['compound']
+    compound_score = scores["compound"]
 
     if compound_score >= 0.05:
         return "Positive"
@@ -47,6 +49,7 @@ def analyze_sentiment(message):
         return "Negative"
     else:
         return "Neutral"
-    
+
+
 if __name__ == "__main__":
     start_tcp_server()
