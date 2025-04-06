73ab06f6212e8e6ebc436f0c8f5d0e32a6ccc873
Paul Le <nthpaul@proton.me>
2025-04-06 23:21:46 +1000

    day 4: hashlib for distributed hashing is better

--- day-4/python-consistent-hashing/consistent-hashing.py
+++ day-4/python-consistent-hashing/consistent-hashing.py
@@ -1,31 +1,12 @@
-class TreeMap:
-    def __init__(self):
-        self.tree = {}
-
-    def put(self, key, value):
-        self.tree[key] = value
-
-    def get(self, key):
-        return self.tree.get(key)
-
-    def lower_key(self, key):
-        lower_keys = [k for k in self.tree if k < key]
-        return max(lower_keys) if lower_keys else None
-
-    def higher_key(self, key):
-        higher_keys = [k for k in self.tree if k > key]
-        return min(higher_keys) if higher_keys else None
-
-    def __repr__(self):
-        return str(self.tree)
+# from sortedcontainers import SortedDict
+import hashlib
 
 
-bst = TreeMap()
+# must use hashlib as it is idempotent across processes, whilst python's built-in hash function is not
+def stable_hash(key: str) -> int:
+    str_bytes = bytes(key, "UTF-8")
+    m = hashlib.md5(str_bytes)
+    return int(m.hexdigest(), base=16)
 
-for i in range(0, 150, 5):
-    bst.put(i, f"Value {i}")
 
-print(bst)
-print(bst.get(50))
-print(bst.lower_key(10))
-print(bst.higher_key(10))
+print("Stable hash of 'hello':", stable_hash("hello"))
